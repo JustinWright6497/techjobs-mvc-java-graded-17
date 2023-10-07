30a554afa1850bba32cbaf92d1625e75cba68a3f
Justin Wright <djw6497@gmail.com>
2023-10-07 01:29:38 -0500

    Completed the assignment. Committing and Pushing to run Actions.

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -1,9 +1,14 @@
 package org.launchcode.techjobsmvc.controllers;
 
+import org.launchcode.techjobsmvc.models.Job;
+import org.launchcode.techjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
 
 import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;
 
@@ -21,7 +26,18 @@ public class SearchController {
         return "search";
     }
 
-    // TODO #3 - Create a handler to process a search request and render the updated search view.
-
+    @PostMapping(value = "results")
+    public String displaySearchResults(Model model, String searchType, String searchTerm) {
+        ArrayList<Job> jobs = new ArrayList<>();
+        if (searchType.equals("All") || searchTerm.equals("all") || searchTerm.isEmpty()) {
+            jobs = JobData.findAll();
+            model.addAttribute("jobs", jobs);
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("jobs", jobs);
+        }
+        model.addAttribute("columns", columnChoices);
+        return "search";
+    }
 }
 
